d142292ba233282433b23be20cfa5c534b39ab54
Akshay Paramanik <152618594+akshay-paramanik@users.noreply.github.com>
2025-05-25 15:39:27 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,16 @@ import Header from './components/headers/Header'
 import Pages from './components/mainpages/Pages'
 import {BrowserRouter as Router } from 'react-router-dom'
 import { DataProvider } from './GlobalState'
+import React, { useContext } from 'react';
 
 function App() {
+  const state = useContext(DataProvider);
+  const [loading] = state.loading;
+
+  if (loading) {
+    return <h2 style={{ textAlign: "center", padding: "20px" }}>Loading...</h2>;
+  }
+
   return (
     <DataProvider>
     <Router>
